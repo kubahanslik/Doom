861c12f8187da2ece217f17451b98edb94abd208
Jakub Hanslík <jakub.hanslik@student.gyarab.cz>
2023-11-17 20:15:04 +0100

    Main file formatting

--- main.cpp
+++ main.cpp
@@ -2,49 +2,49 @@
 
 
 Engine::Engine() :
-		window(SDL_CreateWindow("Raycaster", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN)),
-		renderer(SDL_CreateRenderer(window, -1, 0)),
-		event(SDL_Event()),
-		map(Map(renderer))
-	{
-		// SDL_SetRelativeMouseMode(SDL_TRUE);
+	window(SDL_CreateWindow("Raycaster", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN)),
+	renderer(SDL_CreateRenderer(window, -1, 0)),
+	event(SDL_Event()),
+	map(Map(renderer))
+{
+	// SDL_SetRelativeMouseMode(SDL_TRUE);
+}
+Engine::~Engine() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+}
+
+void Engine::handleEvents() {
+	while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT)
+			running = false;
+
 	}
-	Engine::~Engine() {
-		SDL_DestroyWindow(window);
-		SDL_DestroyRenderer(renderer);
+}
+
+void Engine::draw() {
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	map.draw();
+
+	SDL_RenderPresent(renderer);
+}
+
+void Engine::update() {
+
+	controlFPS();
+}
+
+void Engine::controlFPS() {
+	Uint64 delta = SDL_GetTicks64() - lastFrame;
+	if (delta < DELTA_TIME) {
+		SDL_Delay(DELTA_TIME - delta);
 	}
+	lastFrame = SDL_GetTicks64();
+}
 
-	void Engine::handleEvents() {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT)
-				running = false;
-
-		}
-	}
-
-	void Engine::draw() {
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-
-		map.draw();
-
-		SDL_RenderPresent(renderer);
-	}
-
-	void Engine::update() {
-
-		controlFPS();
-	}
-
-	void Engine::controlFPS() {
-		Uint64 delta = SDL_GetTicks64() - lastFrame;
-		if (delta < DELTA_TIME) {
-			SDL_Delay(DELTA_TIME - delta);
-		}
-		lastFrame = SDL_GetTicks64();
-	}
-
-	bool Engine::isRunning() { return running; }
+bool Engine::isRunning() { return running; }
 
 
 int main(int argc, char* args[]) {
